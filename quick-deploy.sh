e772d4b134291f618e78c99ab17e8b28926397a7
Gie Spaepen <gie.spaepan@zna.be>
2017-11-03 09:59:38 +0100

    Updated travis procedures

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -5,8 +5,8 @@ MESSAGE=$1
 yarn build
 git add -A
 git commit -m "$MESSAGE"
-npm version patch -m "$MESSAGE"
 git push
 git checkout master
 git merge development
 git push github master
+git checkout development
