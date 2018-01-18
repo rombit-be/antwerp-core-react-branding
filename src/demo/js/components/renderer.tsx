ebe2dff0fe097ff7438f12de11068d92fd484edc
Gie Spaepen <gie.spaepan@zna.be>
2018-01-18 12:30:42 +0100

    Removed demo project

--- src/demo/js/components/renderer.tsx
+++
@@ -1,7 +0,0 @@
-import * as ReactDOM from "react-dom";
-
-export default function renderer(element: JSX.Element, id: string) {
-    if (element) {
-        ReactDOM.render(element, document.getElementById(id));
-    }
-}
--- src/demo/js/index.tsx
+++
@@ -1,8 +0,0 @@
-import "./index.scss";
-
-import * as React from "react";
-
-import Hero from "./components/hero";
-import renderer from "./components/renderer";
-
-renderer(<Hero />, "hero");
